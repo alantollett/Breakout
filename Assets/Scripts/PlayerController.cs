7988e486ab01e339c3a9a2803d63117679bfccb5
alantollett <alantollett@outlook.com>
2021-02-26 13:27:56 +0000

    Player can now move from left to right

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 10f;
+    private Vector2 currentMove;
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        currentMove = context.ReadValue<Vector2>();
+    }
+
+    public void Update()
+    {
+        Vector3 moveVelocity = movementSpeed * (currentMove.x * Vector3.right + currentMove.y * Vector3.forward);
+        Vector3 moveThisFrame = Time.deltaTime * moveVelocity;
+        transform.position += moveThisFrame;
+    }
+}
