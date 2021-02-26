70e497d346208763b24ce70ecf9c065f5a4575ef
alantollett <alantollett@outlook.com>
2021-02-26 15:49:17 +0000

    Added a ball prefab with bouncy surface no friction

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class BallController : MonoBehaviour
+{
+    [SerializeField] private float initialSpeed = 2f;
+    private Rigidbody2D rb;
+
+    public void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void OnEnable()
+    {
+        Vector2 velocity = new Vector2(Random.Range(-initialSpeed, initialSpeed), Random.Range(-initialSpeed, initialSpeed));
+        rb.velocity = velocity;
+    }
+
+}
