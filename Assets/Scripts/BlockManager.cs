cb0be120f6dbd6b7177fa9a8526439d33636fcb8
alantollett <alantollett@outlook.com>
2021-02-26 20:53:35 +0000

    Reset camera after unity freeze

--- Assets/Scripts/BlockManager.cs
+++ Assets/Scripts/BlockManager.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class BlockManager : MonoBehaviour
 {
+
+    [SerializeField] private GameObject blockPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int y = 4; y < 6; y += 1)
+        {
+            for (int x = -9; x < 9; x += 2)
+            {
+                Instantiate(blockPrefab, new Vector3(x, y, 0), Quaternion.identity);
+            }
+        }
+
     }
 
     // Update is called once per frame
