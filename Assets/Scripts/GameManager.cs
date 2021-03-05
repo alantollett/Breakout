f15b9ef83e75e08eb377386398023e5e7bbaa232
alantollett <alantollett@outlook.com>
2021-03-05 14:19:28 +0000

    Added functionality to the play again button

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
@@ -7,16 +8,22 @@ public class GameManager : MonoBehaviour {
     [SerializeField] private Text scoreText;
     [SerializeField] private int lives = 5;
     [SerializeField] private Transform ball;
+    [SerializeField] private GameObject gameOverPanel;
     private int score = 0;
 
     public void Start() {
         livesText.text = "Lives: " + lives;
         scoreText.text = "Score: " + score;
+        gameOverPanel.SetActive(false);
     }
 
     public void removeLife() {
         lives -= 1;
         livesText.text = "Lives: " + lives;
+
+        if(lives == 0) {
+            gameOverPanel.SetActive(true);
+        }
     }
     public void addScore(int score) {
         this.score += score;
@@ -25,5 +32,10 @@ public class GameManager : MonoBehaviour {
         if(this.score % 10 == 0) {
             ball.gameObject.GetComponent<BallController>().speedUp();
         }
+    }
+
+    public void restartGame() {
+        Debug.Log("Restarting");
+        SceneManager.LoadScene("Level1");
     }
 }
