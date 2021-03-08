4dbc69af6229af618c6e6d64f71036271cb10dc2
alantollett <alantollett@outlook.com>
2021-03-08 08:31:02 +0000

    Added a main menu

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+    public void startGame() {
+        SceneManager.LoadScene("Level1");
+    }
+    public void quitGame() {
+        Application.Quit();
+    }
+
+}
